f7b7face98db9178b451dfe639958bc96f722ffb
imranhassan87 <imranhassan926@gmail.com>
2021-03-09 08:15:29 +0500

    server completed

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,8 @@
 const db = require('./db');
+const {PubSub} = require('graphql-subscriptions')
 
+const pubsub = new PubSub()
+const MESSAGE_ADDED = 'MESSAGE_ADDED'
 function requireAuth(userId) {
   if (!userId) {
     throw new Error('Unauthorized');
@@ -17,8 +20,16 @@ const Mutation = {
   addMessage: (_root, {input}, {userId}) => {
     requireAuth(userId);
     const messageId = db.messages.create({from: userId, text: input.text});
-    return db.messages.get(messageId);
+    const message = db.messages.get(messageId);
+    pubsub.publish(MESSAGE_ADDED,{messageAdded:message})
+    return message
   }
 }
 
-module.exports = { Query, Mutation };
+const Subscription = {
+  messageAdded :{
+    subscribe:() => pubsub.asyncIterator(MESSAGE_ADDED)
+  }
+}
+
+module.exports = { Query, Mutation, Subscription };
